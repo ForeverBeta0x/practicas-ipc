cb34d32ab90ce33a2eb5789ec4ba403611de79dc
ForeverBeta0x <university.etsinf@gmail.com>
2015-04-19 23:33:29 +0200

    Primer commit

---
+++ Recetario_Final/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Recetario_principal.fxml"));
+			Scene scene = new Scene(root,600,555);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("images/icon.png"));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
