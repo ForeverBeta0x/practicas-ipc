368ac66e8b356edd63600902deceed2e4543ca0e
ForeverBeta0x <university.etsinf@gmail.com>
2015-04-19 23:47:53 +0200

    dani come caca

--- Recetario_Final/src/application/Main.java
+++ Recetario_Final/src/application/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
-//come caca
+//dani come caca
 
 public class Main extends Application {
 	@Override
